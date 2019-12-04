a6db4582141a1cefa33e76c816ed2b538cbcbfaf
MathiasGN <math964g@edu.ucl.dk>
2019-12-04 10:06:29 +0100

    deleted backup

--- styleguide - Kopi/javascript.js
+++
@@ -1,9 +0,0 @@
-//FOR BURGER MENU
-function burgerClick(x) {
-  x.classList.toggle("change");
-    openNav();
-}
-
-function openNav() {
-  document.getElementById("burgerPopout").classList.toggle("active");
-}
