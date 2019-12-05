1b3efa7e30089161ddac240e9ca367e8855321e1
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-12-05 20:13:02 +0100

    Tried to do js but it wont work, so

--- js/carinfo.js
+++ js/carinfo.js
@@ -1,5 +1,20 @@
+console.log("carinfo.js loaded successfully!");
+
 // Click on car element
+// document.getElementById("carLeft").addEventListener("click", createNote);
+
 // Get cursor position
+function cursorPosition(event) {
+  console.log("Getting coords");
+  let posX = event.clickX;
+  let posY = event.clickY;
+  console.log("pos X = " + posX + " /// " + "pos Y = " + posY);
+}
+
+function createNote(event) {
+  console.log("Ur click works");
+  cursorPosition(event);
+}
 // Create Dot at cursor position
 // Create note embedded inside Dot
 // Make the note able to have user input and save it
