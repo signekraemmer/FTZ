4cbe0848a7cbe556b5ab46aa0fbd9ef49a044e5e
MathiasGN <math964g@edu.ucl.dk>
2019-12-06 09:42:42 +0100

    fixed - spelling error, idiot

--- js/carinfo.js
+++ js/carinfo.js
@@ -1,13 +1,13 @@
 console.log("carinfo.js loaded successfully!");
 
 // Click on car element
-// document.getElementById("carLeft").addEventListener("click", createNote);
+document.getElementById("carLeft").addEventListener("click", createNote);
 
 // Get cursor position
-function cursorPosition(event) {
+function cursorPosition() {
   console.log("Getting coords");
-  let posX = event.clickX;
-  let posY = event.clickY;
+  let posX = event.clientX;
+  let posY = event.clientY;
   console.log("pos X = " + posX + " /// " + "pos Y = " + posY);
 }
 
@@ -15,6 +15,7 @@ function createNote(event) {
   console.log("Ur click works");
   cursorPosition(event);
 }
+
 // Create Dot at cursor position
 // Create note embedded inside Dot
 // Make the note able to have user input and save it
