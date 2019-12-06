dd1d93aa201214fba057386717af622a452e2c73
MathiasGN <math964g@edu.ucl.dk>
2019-12-06 13:04:29 +0100

    Click function works

--- js/carinfo.js
+++ js/carinfo.js
@@ -1,19 +1,25 @@
 console.log("carinfo.js loaded successfully!");
 
 // Click on car element
-document.getElementById("carLeft").addEventListener("click", createNote);
+document.getElementById("carLeft").addEventListener("click", cursorPosition);
 
 // Get cursor position
 function cursorPosition() {
-  console.log("Getting coords");
   let posX = event.clientX;
   let posY = event.clientY;
-  console.log("pos X = " + posX + " /// " + "pos Y = " + posY);
+  console.log("X = " + posX + " --- " + "Y = " + posY);
+  createNote(posX, posY);
 }
 
-function createNote(event) {
-  console.log("Ur click works");
-  cursorPosition(event);
+function createNote(posX, posY) {
+  console.log(posX + " + " + posY);
+  // Get's the height of the navbar
+  let h1 = document.getElementsByClassName("logo");
+  let circle = document.getElementsByClassName("noteCircle");
+  console.log(circle);
+  circle[0].style.left = posX + "px";
+  // Subtracts the navbar height, to account for the offset
+  circle[0].style.top = posY - h1[0].offsetHeight + "px";
 }
 
 // Create Dot at cursor position
