af847b4ecdc245748f9c8f1bf39aa07d35d0cb17
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-12-07 13:21:59 +0100

    can add up to more than one note now

--- js/carinfo.js
+++ js/carinfo.js
@@ -11,17 +11,37 @@ function cursorPosition() {
   createNote(posX, posY);
 }
 
-function createNote(posX, posY) {
-  console.log(posX + " + " + posY);
-  // Get's the height of the navbar
-  let h1 = document.getElementsByClassName("logo");
-  let circle = document.getElementsByClassName("noteCircle");
-  console.log(circle);
-  circle[0].style.left = posX + "px";
-  // Subtracts the navbar height, to account for the offset
-  circle[0].style.top = posY - h1[0].offsetHeight + "px";
-}
-
 // Create Dot at cursor position
 // Create note embedded inside Dot
+function createNote(posX, posY) {
+  // Get's the height of the navbar
+  let topbar = document.getElementsByClassName("logo");
+
+  // Subtracts the navbar height, to account for the offset
+  posY = posY - topbar[0].offsetHeight;
+  // HTML note variable
+  let newNote = '<div class="noteCircle" style="top:' + posY + 'px; left:' + posX + 'px;"><div class="noteInfo"></div></div>';
+
+  // Inserts a new note inside the defined container
+  let noteContainer = document.getElementsByClassName("noteContainer")[0];
+  noteContainer.innerHTML += newNote;
+  let notes = document.getElementsByClassName("noteCircle");
+  console.log(notes);
+}
+
+// Click on a note
+// TODO: ADD A QUERY https://flaviocopes.com/add-click-event-to-dom-list/
+let notes = document.getElementsByClassName("noteCircle");
+console.log(notes);
+
+for (let i = 0; i < notes.length; i++) {
+  notes[i].addEventListener("click", openNote);
+}
+
+// Open the note
+function openNote() {
+  console.log("OpenNote");
+  console.log(notes);
+}
+
 // Make the note able to have user input and save it
