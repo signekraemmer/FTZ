229a6bd4329e701dfcc88fa03a29ec857ba89107
MathiasGN <math964g@edu.ucl.dk>
2019-12-09 11:18:52 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ console.log("main.js loaded ");
 document.getElementsByClassName("burger")[0].addEventListener("click", burgerListSlide);
 
 function burgerListSlide() {
-  console.log("success");
   document.getElementsByClassName("magicBox")[0].classList.toggle("burgerActive");
 }
