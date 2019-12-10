c53538f2ae4a930f924288cf552ebf2d3c76a6b5
MathiasGN <math964g@edu.ucl.dk>
2019-12-10 16:43:09 +0100

    new issues

--- js/carinfo.js
+++ js/carinfo.js
@@ -2,6 +2,7 @@ console.log("carinfo.js loaded successfully!");
 
 // This variable holds all the created notes
 let notes = document.getElementsByClassName("noteCircle");
+let notesInfo = document.getElementsByClassName("noteInfo");
 
 // Click on car element
 document.getElementById("carLeft").addEventListener("click", cursorPosition);
@@ -28,10 +29,16 @@ function createNote(posX, posY) {
   // Inserts a new note inside the defined container
   let noteContainer = document.getElementsByClassName("noteContainer")[0];
   noteContainer.innerHTML += newNote;
-
+// TODO: Maybe when you add eventlistener when you spawn the nate, it's possible to insert the child with innerHTML, allowing you to addevenetlistineer for the blue note.
   // TODO: This can probably be done when the note is spawned.
   for (let i = 0; i < notes.length; i++) {
-    notes[i].addEventListener("click", openNote);
+    notes[i].addEventListener("mouseover", openNote);
+    notesInfo[i].addEventListener("mouseout", openNote);
+    notes[i].addEventListener("click", editNote);
+  }
+
+  for (let i = 0; i < notes.length; i++) {
+    notesInfo[i].addEventListener("click", editNote);
   }
 }
 
@@ -48,9 +55,12 @@ function createNote(posX, posY) {
 // Open the note
 function openNote() {
   console.log("OpenNote");
-  console.log(event);
   // Crazy hyped over "this". The "this" is the element you clicked on, and it's able to identify it like so.
   this.firstElementChild.classList.toggle("active");
+}
+
+function editNote() {
+  console.log("Editing note");
 }
 
 // TODO: Remove the "active" class from non-focused notes? This could cover the if-else requirement.
