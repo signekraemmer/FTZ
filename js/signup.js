fd7ccacb378d0eb2b1c0bb8ba5cbb49fba972ad7
Signe Kræmmer <signekraemmer@hotmail.com>
2019-12-10 21:43:59 +0100

    js virker!!!!!

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,18 @@
-let sBtn = document.getElementbyId("signupBtn");
+//
+document.getElementById("signupBtn").addEventListener("click", checkInputs);
+
+// This variable gets the inputfields
+let inp = document.getElementsByTagName("input");
+
+//Checks the rest of the inputfields
+function checkInputs() {
+  for (let i = 0; i < inp.length; i++) {
+    if (inp[i].value == "") {
+
+      alert("Indtast venligst " + inp[i].placeholder);
+
+    } else {
+      window.location.href = "frontpage.html";
+    }
+  }
+}
