30aa5ea3c770d0e9dbbf1d8db5d99b712d7bd506
MathiasGN <math964g@edu.ucl.dk>
2019-12-10 13:25:58 +0100

    Closer to fix? for search

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ document.getElementsByClassName("burger")[0].addEventListener("click", burgerLis
 function burgerListSlide() {
   document.getElementsByClassName("magicBox")[0].classList.toggle("burgerActive");
 }
+
+let seaBox = document.getElementsByClassName('searchBox')[0];
+let searchIcon = document.getElementById("activateSearchBox");
+
+searchIcon.addEventListener('click', openSearch);
+
+function openSearch() {
+  seaBox.classList.toggle("searchActive");
+}
