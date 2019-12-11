02304274ba71480844305cca72bc8690b7389af2
Charlottebknudsen <54641776+Charlottebknudsen@users.noreply.github.com>
2019-12-11 16:00:28 +0100

    if, else, hvis man ikke udfylder formlen = alert

--- js/regninger.js
+++ js/regninger.js
@@ -29,15 +29,31 @@ document.querySelector('input[type="button"]').addEventListener('click', newBill
 document.getElementById("closeRegninger").addEventListener('click', newBill);
 function newBill() {
   let vaerksted = document.getElementById('vaerkstedsNavn').value;
-  let pris = document.getElementById('vaerkstedPris').value + ' kr.';
+  let pris = document.getElementById('vaerkstedPris').value;
   console.log(vaerksted);
   console.log(pris);
 
+if (vaerksted == "" || pris == "") {
+console.log("error");
+alert("Hej din ged");
+}
+
+else {
   let newListItem = document.createElement('li');
-  newListItem.innerHTML = vaerksted + pris;
+
+  newListItem.innerHTML = vaerksted + '<span class="prisen">' + pris + " kr." + '</span>';
 
  document.querySelector('.regningList').appendChild(newListItem);
+
+
+   setTimeout(function(){ alert("hejj"); }, 2000);
 }
+
+
+ }
+
+
+
 
 // document.querySelector('section').addEventListener('click', function(event) {
 //  if (event.target.tagName.toLowerCase() === 'li') {
